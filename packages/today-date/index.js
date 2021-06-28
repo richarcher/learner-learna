46ac31fb477bb6f19f3fac1e9de25f1d2eab7fa5
richarcher <richarcher@users.noreply.github.com>
2021-06-28 15:41:04 +0200

    refactor date helper

--- packages/today-date/index.js
+++ packages/today-date/index.js
@@ -2,7 +2,10 @@
 
 module.exports = todayDate;
 
-function todayDate() {
+function todayDate(name) {
   const date = new Date();
+  if (name) {
+    return `Hello, ${name}, the time and date is ${date}`;
+  }
   return `The time and date is ${date}!`;
 }
